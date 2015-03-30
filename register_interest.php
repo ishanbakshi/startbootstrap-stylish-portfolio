43679d430a89a155c2af7f49eb54bd7de945c8a8
ishan bakshi <ishan.bakshi@gmail.com>
2015-03-30 16:31:17 +1100

    include settings for database connection

--- register_interest.php
+++ register_interest.php
@@ -1,4 +1,5 @@
 <?php
+	require_once ("db-settings.php");
 	$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
 	if(!$conn){
 		//display an error
@@ -7,10 +8,8 @@
 		//on successfull connection choose table
 		$sql_table = "registeration";
 		$email = trim($_POST["email"]);
-		$comment = trim($_POST["comment"]);
-
-		//set up sql command
-		$query = "insert into '$sql_table' ('email','comments') values ('adaad@sdsd.com', 'adsd sdsd sdsd')"; 
+		$comment = trim($_POST["comment"]); 
+		$query = "INSERT INTO registeration (email, comments) VALUES ('$email','$comment')";
 		$result = mysqli_query($conn, $query); 
 		if(!$result) { 
 			echo "<p class=\"wrong\">Something is wrong with ", $query, "</p>";
